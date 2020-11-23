8f5a72ed3ef26ab1792ac816686ec562164041f3
acepitcher2009 <69870798+acepitcher2009@users.noreply.github.com>
2020-11-23 15:00:30 -0600

    Adding to single page app

--- src/components/mainComponent.js
+++ src/components/mainComponent.js
@@ -7,6 +7,10 @@ import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
 import { Switch, Route, Redirect } from 'react-router-dom';
+import Contact from './ContactComponent';
+import { COMMENTS } from '../shared/comments';
+import { PARTNERS } from '../shared/partners';
+import { PROMOTIONS } from '../shared/promotions';
 
 
 class Main extends Component {
@@ -14,24 +18,32 @@ class Main extends Component {
         super(props);
         this.state = {
             campsites: CAMPSITES,
-            selectedCampsite: null
+            comments: COMMENTS,
+            partners: PARTNERS,
+            promotions: PROMOTIONS
         };
     }
     onCampsiteSelect(campsiteId) {
         this.setState({ selectedCampsite: campsiteId });
     }
     render() {
-        const Homepage = () => {
+        const HomePage = () => {
             return (
-                <Home />
+                <Home
+                    campsite={this.state.campsites.filter(campsite => campsite.featured)[0]}
+                    promotion={this.state.promotions.filter(promotion => promotion.featured)[0]}
+                    partner={this.state.partners.filter(partner => partner.featured)[0]}
+                />
             )
         }
         return (
             <div>
                 <Header />
                 <Switch>
-                    <Directory campsites={this.state.campsites} onClick={campsiteId => this.onCampsiteSelect(campsiteId)} />
-                    <CampsiteInfo campsite={this.state.campsites.filter(campsite => campsite.id === this.state.selectedCampsite)[0]} />
+                    <Route path='/home' component={HomePage} />
+                    <Route exact path='/directory' render={() => <Directory campsites={this.state.campsites} />} />
+                    <Route exact path='/contactus' component={Contact} />
+                    <Redirect to='/home' />
 
                 </Switch>
                 <Footer />
