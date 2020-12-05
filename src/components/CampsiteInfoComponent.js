2462ceac011067cf141030c99471e1a271f0fe80
acepitcher2009 <69870798+acepitcher2009@users.noreply.github.com>
2020-12-05 12:53:05 -0600

    Week 4 Assignment

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,7 +1,6 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem, Button, Modal, ModalBody, ModalHeader } from 'reactstrap';
 import { Link } from 'react-router-dom';
-import { Component } from 'react';
 import { Control, Errors, LocalForm } from 'react-redux-form';
 import Label from 'reactstrap/lib/Label';
 
