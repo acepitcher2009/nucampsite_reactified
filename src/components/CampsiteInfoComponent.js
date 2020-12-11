f0cb7198849c35003b3f80b73043a353f6391989
acepitcher2009 <69870798+acepitcher2009@users.noreply.github.com>
2020-12-11 09:55:31 -0600

    React Transistions

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -4,13 +4,14 @@ import { Link } from 'react-router-dom';
 import { Control, Errors, LocalForm } from 'react-redux-form';
 import Label from 'reactstrap/lib/Label';
 import { Loading } from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl';
 
 function RenderCampsite({ campsite }) {
     if (campsite) {
         return (
             <div className="col-md-5 m-1">
                 <Card>
-                    <CardImg top src={campsite.image} alt={campsite.name} />
+                    <CardImg top src={baseUrl + campsite.image} alt={campsite.name} />
                     <CardBody>
                         <CardText>{campsite.description}</CardText>
                     </CardBody>
@@ -21,7 +22,7 @@ function RenderCampsite({ campsite }) {
     return <div />;
 }
 
-function RenderComments({ comments, addComment, campsiteId }) {
+function RenderComments({ comments, postComment, campsiteId }) {
     if (comments) {
         return (
             <div className="col-md-5 m-1">
@@ -34,7 +35,7 @@ function RenderComments({ comments, addComment, campsiteId }) {
 
                     );
                 })}
-                <CommentForm campsiteId={campsiteId} addComment={addComment} />
+                <CommentForm campsiteId={campsiteId} postComment={postComment} />
             </div>
 
         )
@@ -78,7 +79,11 @@ function CampsiteInfo(props) {
                 </div>
                 <div className="row">
                     <RenderCampsite campsite={props.campsite} />
-                    <RenderComments comments={props.comments} addComment={props.addComment} campsiteId={props.campsite.id} />
+                    <RenderComments
+                        comments={props.comments}
+                        postComment={props.postComment}
+                        campsiteId={props.campsite.id}
+                    />
                 </div>
             </div>
         );
@@ -103,8 +108,8 @@ class CommentForm extends Component {
                 author: false
             }
         };
-        this.modalToggle = this.modalToggle.bind(this);
-        this.submitHandler = this.submitHandler.bind(this);
+        this.toggleModal = this.toggleModal.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
 
@@ -116,7 +121,7 @@ class CommentForm extends Component {
 
     handleSubmit(values) {
         this.toggleModal();
-        this.props.addComment(this.props.campsiteId, values.rating, values.author, values.text)
+        this.props.postComment(this.props.campsiteId, values.rating, values.author, values.text);
 
     }
 
